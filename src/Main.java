42b3d1ae4767739c4607cf34563fb200edafe87b
Alyk Lippincott <lippinay@mail.uc.edu>
2024-09-24 16:54:48 -0400

    Completed Part 1

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int intOperandA = 5;
+        int intOperandB = 13;
+        int intSum;
+        int intProduct;
+        int intDifference;
+        int intQuotient;
+        int intModulo;
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum using ints of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        double doubleOperandA = 3.50;
+        double doubleOperandB = 4.75;
+        double doubleSum;
+        double doubleProduct;
+        double doubleDifference;
+        double doubleQuotient;
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+
+        double myLunchCost = 12.50;
+        int numberOfKids = 2;
+        boolean isRaining = false;
+        double gasPrice = 3.89;
+        int favoriteNumber = 7;
+        double shoeSize = 10.5;
+        int birthMonth = 4;
+        String fullName = "Alyk Lippincott";
+
+
+    }
+}
